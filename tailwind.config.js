7f19bc6a1d3a5a4d498a528d9002c2cdb774ff34
eugeniaGn <l.gore020501@itses.edu.mx>
2023-07-05 09:06:12 -0600

    Add color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'blanco': '#F2F2F2',
+        'blanco': '#FFFFFF',
+        'witheSmoke': '#F2F2F2',
+        'timberWolf': '#E1DCD6',
         'grisC': '#E3E3E3',
         'grisF': '#D9D9D9',
-        'negro': '#555555',
-        'verdeC': '#C1DDB7',
-        'verdeF': '#278617',
+        'silver': '#C5B6B1',
+        'teaGreen': '#C1DDB7',
+        'indiaGreen': '#278617',
+        'imperialRed': '#EF4444',
+        'teaRed': '#FFCCCC',
+        'coralPink': '#DC9788',
+        'davysGray': '#545454',
       },
       fontFamily: {
         'lato': ['"Lato"', 'sans-serif']
