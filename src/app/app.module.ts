9188573fcb9b1b50c3807dfb0032c8dc6408e676
Sebastian Gutierrez <l.gufs980514@itses.edu.mx>
2023-07-15 14:19:30 -0600

    Add module packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy, RouterModule } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CommonModule } from '@angular/common';
 import { InicioComponent } from './inicio/inicio.component';
 import { CrearOutfitComponent } from './crear-outfit/crear-outfit.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { ErrorInterceptor } from './services/error.interceptors';
+import { AgregarPrendaComponent } from './agregar-prenda/agregar-prenda.component';
+import { PhotoService } from './services/photo.service';
+import { CloudinaryModule } from '@cloudinary/ng';
+import {AutocompleteLibModule} from 'angular-ng-autocomplete';
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
-    CrearOutfitComponent
+    CrearOutfitComponent,
+    AgregarPrendaComponent
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(),
+    IonicModule.forRoot({mode: 'ios'}),
     AppRoutingModule,
     CommonModule,
     HttpClientModule,
-    RouterModule
+    RouterModule,
+    CloudinaryModule,
+    AutocompleteLibModule
   ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    PhotoService,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    // Multi:true agrega funcionalidad al provider existente, en vez de sobreescribirlo
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
