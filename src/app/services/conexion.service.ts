8238f963813ce7ce34f8d4630722cec83435ff0f
Sebastian Gutierrez <l.gufs980514@itses.edu.mx>
2023-07-15 14:20:08 -0600

    URL changes

--- src/app/services/conexion.service.ts
+++ src/app/services/conexion.service.ts
@@ -1,11 +1,13 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment as env } from '../../environments/environment';
+import { environment as prod } from '../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConexionService {
-  baseUrl = '';
+  baseUrl = env.baseServicesURL;
 
   public httpOptions = {
     headers: new HttpHeaders({
@@ -19,10 +21,10 @@ export class ConexionService {
   ) { }
 
   get(modelo: string, accion: string){
-    return this.http.get(`${this.baseUrl}${modelo}.php?opcion=${accion}`);
+    return this.http.get(`${this.baseUrl}${modelo}.php?option=${accion}`);
   }
 
   post(modelo: string, accion: string, datos: any){
-    return this.http.post(`${this.baseUrl}${modelo}.php?opcion=${accion}`, datos);
+    return this.http.post(`${this.baseUrl}${modelo}.php?option=${accion}`, datos);
   }
 }
