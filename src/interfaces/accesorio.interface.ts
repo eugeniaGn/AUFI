c11dbcabbf8746425961b0833ba8b1faee28c6bf
Sebastian Gutierrez <l.gufs980514@itses.edu.mx>
2023-07-18 16:01:19 -0600

    Create interfaces

---
+++ src/interfaces/accesorio.interface.ts
@@ -0,0 +1,10 @@
+export interface Accesorio {
+    tipo?: number,
+    subtipo?: number,
+    carac?: number,
+    estilo?: number,
+    color?: number,
+    material?: number,
+    marca?: number
+    imagen?: string
+}
---
+++ src/interfaces/prenda.interface.ts
@@ -0,0 +1,11 @@
+export interface Prenda {
+    tipo?: number,
+    subtipo?: number,
+    carac?: number,
+    estilo?: number,
+    clima?: number,
+    color?: number,
+    material?: number,
+    marca?: number
+    imagen?: string
+}
