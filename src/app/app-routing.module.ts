291fde73cb542e77811a97168064aa46d37cc150
eugeniaGn <l.gore020501@itses.edu.mx>
2023-07-25 12:33:34 -0600

    Change default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,7 +16,7 @@ const routes: Routes = [
   {path: 'login', component: LoginComponent, },
   {
     path: '**',
-    redirectTo: 'login',
+    redirectTo: 'agregarPrenda',
     pathMatch: 'full'
   },
   {
