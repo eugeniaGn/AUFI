d0d4a65f515c849bc75086924a74420d7035eaa9
eugeniaGn <l.gore020501@itses.edu.mx>
2023-08-29 13:20:58 -0600

    Create component

--- src/app/crear-outfit/crear-outfit.component.ts
+++ src/app/crear-outfit/crear-outfit.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { DialogElegirPrendaComponent } from '../dialog-elegir-prenda/dialog-elegir-prenda.component';
+import { ConexionService } from '../services/conexion.service';
 
 @Component({
   selector: 'app-crear-outfit',
@@ -13,21 +14,51 @@ export class CrearOutfitComponent  implements OnInit {
   gusta:boolean = false;
   guardado:boolean = false;
 
+  contenedores = new Array(12);
+
+  prenda: any;
+  outfit: any = {prendas: []};
+  prendas: any[]  = [];
+
+
   constructor(
     private router: Router,
-    public dialog: MatDialog
-  ) { }
+    public dialog: MatDialog,
+    private conx: ConexionService
+  ) {
+  }
+
+  async getData() {
+    await this.obtenerPrendas();
+    await this.obtenerAccesorios();
+  }
 
   ngOnInit() {}
 
-  openDialogElegir(data: any) {
-    console.log(data);
-    let dialogRef = this.dialog.open(DialogElegirPrendaComponent, { width: '85%', height: '85%', data }
-    );
-
-    dialogRef.afterClosed().subscribe(result => {
-      // console.log(`Dialog result: ${result}`);
+  async obtenerPrendas() {
+    this.conx.get('prenda', 'getPrendas').subscribe((data: any) => {
+      this.prendas = data;
     });
+  }
+
+  async obtenerAccesorios() {
+    this.conx.get('accesorio', 'getAccesorios').subscribe((dato: any) => {
+      dato.forEach((accesorio: any) =>{
+        this.prendas.push(accesorio);
+      });
+    });
+  }
+
+  openDialogElegir(index: any) {
+
+      let dialogRef = this.dialog.open(DialogElegirPrendaComponent, { width: '100%', height: '100%'}
+      );
+
+      dialogRef.afterClosed().subscribe(result => {
+        this.contenedores[index] = result;
+        this.outfit.prendas.push(result?.idPrenda);
+      });
+
   }
 
   loUse(){
@@ -46,12 +77,28 @@ export class CrearOutfitComponent  implements OnInit {
     }
   }
 
-  guardar(){
+  agregarColeccion(){
     if(this.guardado == false){
       this.guardado = true;
     }else{
       this.guardado = false;
     }
+  }
+
+  quitarPrenda(index: any){
+    this.contenedores[index] = '';
+  }
+
+  guardarOutfit() {
+    this.conx.post('outfit', 'addOutfits', {response: {outfits: [this.outfit]}}).subscribe((data: any) => {
+      if (data) {
+        alert('Outfit agregado!');
+        this.contenedores.forEach(contenedor => {
+          contenedor.splice(0);
+        });
+        this.outfit.splice(0);
+      }
+    })
   }
 
 }
